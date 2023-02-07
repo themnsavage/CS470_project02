b40a0fef4a2116e378a99c2edf07a5277bdda3f9
themnsavage <mnsavage@crimson.ua.edu>
2023-02-07 16:43:59 -0600

    feat(Inputs): add delete and find input options

--- src/App.js
+++ src/App.js
@@ -6,17 +6,36 @@ import BTree from './BTree';
 
 function App() {
   const [insertInput, setInsert] = useState('');
+  const [deleteInput, setDelete] = useState('');
+  const [findInput, setFind] = useState('');
 
   const binaryTreeRef = useRef();
   const bTreeRef = useRef();
 
   useEffect(() => {
     if(insertInput != ''){
+      console.log(`insert input: ${insertInput}`);
       binaryTreeRef.current.insert(insertInput);
       bTreeRef.current.insert(insertInput);
     }
     setInsert('');
   },[insertInput]);
+
+  useEffect(() => {
+    if(deleteInput != ''){
+      console.log(`delete input: ${deleteInput}`);
+    }
+    setDelete('');
+  },[deleteInput]);
+
+  useEffect(() => {
+    if(findInput != ''){
+      console.log(`find input: ${findInput}`);
+    }
+    setFind('');
+  },[findInput]);
+
+
 
   return (
     <div className="App">
@@ -24,7 +43,7 @@ function App() {
         <h1>Data Structures Visuals</h1>
       </div>
       <h1>Inputs:</h1>
-      <Inputs setInsert={setInsert}/>
+      <Inputs setInsert={setInsert} setDelete={setDelete} setFind={setFind}/>
       <h2>Binary Tree:</h2>
       <BinaryTree ref={binaryTreeRef}/>
       <h2>B-Tree:</h2>
--- src/Inputs.js
+++ src/Inputs.js
@@ -1,17 +1,29 @@
 import { useState } from "react";
 
-const Inputs = ({setInsert}) => {
-    const [insertValue, setInsertValue] = useState('')
+const Inputs = ({setInsert, setDelete, setFind},) => {
+    const [inputValue, setInputValue] = useState('');
 
-    const handleClick = () => {
-        setInsert(insertValue);
-        setInsertValue('')
+    const handleClick = (event) => {
+        if(event.target.id == 'insertButton'){
+            setInsert(inputValue);
+            setInputValue('')
+        }        
+        if(event.target.id == 'deleteButton'){
+            setDelete(inputValue);
+            setInputValue('')
+        }
+        if(event.target.id == 'findButton'){
+            setFind(inputValue);
+            setInputValue('')
+        }
     }
 
     return (
         <div>
-            <input type ="text" id="insert" placeholder="Enter a value to be added" value= {insertValue} onChange={(e) => setInsertValue(e.target.value)}/>
-            <button id="insertClick" onClick={handleClick}> Insert </button>
+            <input type ="text" placeholder="Enter a input here" value= {inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+            <button id='insertButton' onClick={handleClick}> Insert </button>
+            <button id='deleteButton' onClick={handleClick}> Delete </button>
+            <button id='findButton' onClick={handleClick}> Find </button>
         </div>
     );
 }
